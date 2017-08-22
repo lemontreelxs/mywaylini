be47ca10685b58f843eee94022f395029fde86c2
lxs <lemontreelxs@live.cn>
2017-08-22 17:53:09 +0800

    aa

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-const routes: Routes = []
+import { FirstPageComponent } from './first-page/first-page.component';
+const routes: Routes = [{ path: 'firstpage', component: FirstPageComponent }]
 @NgModule({
   imports: [
     RouterModule.forRoot(routes)
