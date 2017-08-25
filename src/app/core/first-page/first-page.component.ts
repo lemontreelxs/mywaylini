8be5a8ca60c3e4cbf56b5c99645991ce15fb0718
lxs <lemontreelxs@live.cn>
2017-08-25 23:42:37 +0800

    a'a

--- src/app/core/first-page/first-page.component.ts
+++ src/app/core/first-page/first-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-first-page',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first-page.component.css']
 })
 export class FirstPageComponent implements OnInit {
+
+  @HostBinding('style.display') display = 'flex';
 
   constructor() { }
 
